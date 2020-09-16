3f25bdb4fdbea16fc33c888544b87b33d333f85b
kkehl-usgs <kkehl@contractor.usgs.gov>
2020-09-16 08:06:59 -0700

    Create aws_s3 extension via lambda (#205)
    * put uuid in json_data table
    
    * update CHANGELOG
    
    * lookup json_data_id from uuid, also pep 8 formatting
    
    * add missing param
    
    * fix bug in query
    
    * some debugging comments
    
    * comment out mogrify for debugging
    
    * learn more about mogrify
    
    * fix bug in query
    
    * remove extra debugging
    
    * add new handling for big S3 objects
    
    * fix typo
    
    * add CHANGELOG
    
    * remove bucket environment variable and use bucket name from event instead
    
    * fix typo
    
    * change string to int for less than comparison
    
    * fix less than comparison
    
    * try creating aws_s3 extension via lambda
    
    * fix bug
    
    * Fix bug
    
    * change cron to monthly

---
+++ src/install.py
@@ -0,0 +1,42 @@
+"""
+ETL loads content S3 and persist it into an RDS
+"""
+import logging
+import os
+
+from .etl.event_processor import TriggerEvent
+from .etl.rds import RDS
+from .etl.config import CONFIG
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def aws_s3():
+    rds = RDS()
+    try:
+        rds.install_aws_s3()
+    except Exception as e:
+        logger.debug(repr(e), exc_info=True)
+        raise RuntimeError(repr(e))
+    finally:
+        rds.disconnect()
+
+
+def lambda_handler(event, context):
+    """
+    Creates the aws_s3 extension for transfering S3 data directly to RDS.
+    :param event: AWS S3 JSON event containing a Record list of messages
+    :param context:
+    :return: success or fail status
+    """
+    response = None
+    try:
+        logger.debug(event)
+        aws_s3()
+    except Exception as e:
+        logger.info(f'About to exit with this exception: {repr(e)}')
+        raise e
+    else:
+        response = {'statusCode': 201, 'body': "aws_s3 extension installed correctly"}
+    return response
